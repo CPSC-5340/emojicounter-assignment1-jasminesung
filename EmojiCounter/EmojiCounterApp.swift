f9b81bd6c480da455eca254e39583e025a838d0e
jasminesung <jasminesung1130@gmail.com>
2024-03-16 22:29:03 -0400

    Initial Commit

---
+++ EmojiCounter/EmojiCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiCounterApp.swift
+//  EmojiCounter
+//
+//  Created by Jasmine Sung on 16/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
